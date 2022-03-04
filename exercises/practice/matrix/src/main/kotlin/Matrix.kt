cd3c4991c7c1b3259bc8e7af3ce4ec3bd3f2e82b
MattiaRiola <s280169@studenti.polito.it>
2022-03-04 18:14:05 +0100

    feat: es Matrix solved with sequence

--- exercises/practice/matrix/src/main/kotlin/Matrix.kt
+++ exercises/practice/matrix/src/main/kotlin/Matrix.kt
@@ -1,10 +1,38 @@
+
+
+
 class Matrix(private val matrixAsString: String) {
 
+    private var matrix: MutableList<MutableList<Int>> = mutableListOf()
+
+    init {
+        matrix = matrixAsString.lines().asSequence().map {
+                it.trim() //clean spaces
+            }
+            .map {
+                it.split(" +".toRegex())
+            }.map { row ->
+                row.asSequence().map {
+                    it.toInt()
+                }.toMutableList()
+
+            }.toMutableList()
+        println(matrix);
+
+
+    }
     fun column(colNr: Int): List<Int> {
-        TODO("Implement this to complete the task")
+        return matrix.map{
+            it[colNr-1]
+        }.toList()
     }
 
     fun row(rowNr: Int): List<Int> {
-        TODO("Implement this to complete the task")
+
+        return  matrix[rowNr-1]
+    }
+
+    fun coordinate(rowNr: Int, colNr: Int): Int {
+        return matrix[colNr][rowNr]
     }
 }
