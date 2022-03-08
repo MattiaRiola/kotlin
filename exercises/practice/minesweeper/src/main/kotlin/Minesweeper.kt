f795b882e342c7a03c0fef02c8a41396d309240b
MattiaRiola <s280169@studenti.polito.it>
2022-03-08 11:29:43 +0100

    feat: Minesweeper exercise solved with mapIndexed

--- exercises/practice/minesweeper/src/main/kotlin/Minesweeper.kt
+++ exercises/practice/minesweeper/src/main/kotlin/Minesweeper.kt
@@ -1,8 +1,39 @@
-data class MinesweeperBoard(val todo: Nothing) {
+data class MinesweeperBoard(val input: List<String>) {
+//    private var output: MutableList<String> = input.toMutableList()
 
-    // TODO: Implement proper constructor
 
     fun withNumbers(): List<String> {
-        TODO("Implement this function to complete the task")
+        val res = input.mapIndexed{ y, row ->
+            val rowRes = row.mapIndexed { x, slot ->
+                if( slot == '*'){
+                    '*'
+                }
+                else {
+                    calcCloseBombs(x, y)
+                }
+            }.joinToString(separator = "")
+            rowRes
+        }
+
+        return res
+
     }
+
+    private fun calcCloseBombs(x: Int, y: Int): Char {
+        var count = 0
+        val xMin = maxOf(0, x - 1)
+        val yMin = maxOf(0, y - 1)
+        val xMax = minOf(x + 1, input[y].lastIndex)
+        val yMax = minOf(y + 1, input.lastIndex)
+        for (i in yMin..yMax)
+            for (j in xMin..xMax)
+                if (input[i][j] == '*')
+                    count++
+        return if (count == 0)
+            ' '
+        else
+            count.toString()[0]
+    }
+
+
 }
