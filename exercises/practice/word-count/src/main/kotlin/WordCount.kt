812c6c8ee81597b88df058346d0f4cfab1d82f2c
MattiaRiola <s280169@studenti.polito.it>
2022-03-11 17:03:11 +0100

    feat: word counter

--- exercises/practice/word-count/src/main/kotlin/WordCount.kt
+++ exercises/practice/word-count/src/main/kotlin/WordCount.kt
@@ -1,6 +1,16 @@
 object WordCount {
 
     fun phrase(phrase: String): Map<String, Int> {
-        TODO("Implement this function to complete the task")
+
+        return phrase.split("[^a-zA-Z0-9']+".toRegex())
+            .map{it.trim('\'').lowercase()}
+            .filter{it.isNotEmpty()}
+            .groupBy { it }
+            .map {
+                Pair(it.key, it.value.size)
+                //or use the key to value syntax:
+                //it.key to it.value.size
+            }.toMap()
     }
+
 }
