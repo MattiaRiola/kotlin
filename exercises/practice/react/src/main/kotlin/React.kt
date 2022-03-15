3cc2867f1674ea46a0fa2d2bad129cf7fe190dc1
MattiaRiola <s280169@studenti.polito.it>
2022-03-15 11:28:48 +0100

    feat: react (still not working)

--- exercises/practice/react/src/main/kotlin/React.kt
+++ exercises/practice/react/src/main/kotlin/React.kt
@@ -1,7 +1,48 @@
+import kotlin.properties.Delegates
+
 class Reactor<T>() {
+
+
+
+    abstract inner class Cell<T>{
+        abstract var value: T
+        var computeCells: MutableList<ComputeCell<T>> = mutableListOf()
+    }
+
+    open inner class InputCell<T>(inputValue: T) : Cell<T>(){
+
+
+        override var value = inputValue
+            set(newVal) {
+                if(newVal != field){
+                    field = newVal
+                    for (computeCell in computeCells) {
+                        computeCell.update()
+                    }
+                }
+            }
+
+    }
+
+    inner class ComputeCell<T>(vararg elements: InputCell<T>, var computator: (List<T>)->T) : InputCell<T>(computator(elements.map{it.value})){
+        val inputs = elements.toList()
+        init {
+            computeCells.add(this)
+        }
+
+        fun update(){
+            val newVal = computator(inputs.map { it.value })
+            if(value != newVal){
+                value = newVal
+                //callBack()
+            }
+        }
+    }
     // Your compute cell's addCallback method must return an object
     // that implements the Subscription interface.
     interface Subscription {
         fun cancel()
     }
 }
+
+
